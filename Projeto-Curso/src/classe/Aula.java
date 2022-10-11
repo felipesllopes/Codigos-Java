041772e42a7e9ecba9c0194f42f132db1056b5f8
Felipe Silva <99768939+felipesllopes@users.noreply.github.com>
2022-10-11 11:07:50 -0300

    Adicionando projeto

---
+++ Projeto-Curso/src/classe/Aula.java
@@ -0,0 +1,25 @@
+package classe;
+
+public class Aula {
+
+	private String nomeAula;
+	private int tempoAula;
+
+	public Aula(String nomeAula, int tempoAula) {
+		this.nomeAula = nomeAula;
+		this.tempoAula = tempoAula;
+	}
+
+	public String getNomeAula() {
+		return nomeAula;
+	}
+
+	public int getTempoAula() {
+		return tempoAula;
+	}
+
+	@Override
+	public String toString() {
+		return nomeAula + ", " + tempoAula + " min";
+	}
+}
